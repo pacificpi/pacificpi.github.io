6189127c2182a96b1ba88a97c11a81ca4b909928
pacificpi <119114171+pacificpi@users.noreply.github.com>
2025-07-10 21:06:35 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // You might set this to '/<repo-name>/' if not at root.
+  base: '/pacificpi.github.io/', // You might set this to '/<repo-name>/' if not at root.
   build: {
     outDir: 'dist', // This is the default, but ensure it's set.
   },
